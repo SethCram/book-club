6685034b173f6e6d5a2f812f09a4e32c07161bf5
sethhi2 <sethcram@gmail.com>
2023-01-22 16:42:35 -0800

    react app created + top bar started on

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import TopBar from "./topbar/TopBar";
+
+function App() {
+  return (
+    <TopBar></TopBar>
+  );
+}
+
+export default App;
---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,7 @@
+import "./TopBar.css"
+
+export default function TopBar() {
+  return (
+    <div className='topbar'>TopBar</div>
+  )
+}
