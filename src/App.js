970d53cca5ddea47cbaff69cca293a394b237af3
sethhi2 <sethcram@gmail.com>
2023-01-23 19:34:52 -0800

    write page complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import SinglePostPage from "./pages/singlepostpage/SinglePostPage";
+import WritePage from "./pages/writepage/WritePage";
 
 function App() {
   return (
     <div>
       <TopBar></TopBar>
-      <SinglePostPage/>
+      <WritePage/>
     </div>
   );
 }
--- src/pages/writepage/WritePage.jsx
+++ src/pages/writepage/WritePage.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import "./WritePage.css"
 
 export default function WritePage() {
   return (
-    <div className='writePage'>WritePage</div>
+      <div className='writePage'>
+          <img 
+              className="writeImg"
+              src="https://upload.wikimedia.org/wikipedia/en/3/37/Dog_with_a_Blog_Logo.png"
+              alt=""
+          />
+          <form className='writeForm'>
+              <div className='writeFormGroup'>
+                  <label htmlFor='fileInput'>
+                    <i className="writeIcon fa-solid fa-plus"></i>
+                  </label>
+                  <input
+                      type='file'
+                      id='fileInput'
+                      style={{ display: "none" }}
+                  />
+                  <input
+                      className='writeInput'
+                      type='text'
+                      placeholder='Title'
+                      autoFocus={true}
+                  />
+              </div>
+              <div className="writeFormGroup">
+                  <textarea className="writeInput writeText" placeholder="Tell your story..." type="text"></textarea>
+              </div>
+              <button className="writeSubmit">Publish</button>
+          </form>
+    </div>
   )
 }
