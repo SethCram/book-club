854e35ed2f945afc16af14670577313e21ef3c69
sethhi2 <sethcram@gmail.com>
2023-01-24 18:57:54 -0800

    login page finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Settings from "./pages/settings/Settings";
 import SinglePostPage from "./pages/singlepostpage/SinglePostPage";
 import WritePage from "./pages/writepage/WritePage";
@@ -8,7 +9,7 @@ function App() {
   return (
     <div>
       <TopBar></TopBar>
-      <Settings />
+      <Login/>
     </div>
   );
 }
