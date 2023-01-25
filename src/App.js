7eb991dc9bee5860c0d6a0da513d3e5f53913787
sethhi2 <sethcram@gmail.com>
2023-01-24 19:05:51 -0800

    register page finished

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Login from "./pages/login/Login";
 import Settings from "./pages/settings/Settings";
 import SinglePostPage from "./pages/singlepostpage/SinglePostPage";
 import WritePage from "./pages/writepage/WritePage";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div>
       <TopBar></TopBar>
-      <Login/>
+      <Register/>
     </div>
   );
 }
