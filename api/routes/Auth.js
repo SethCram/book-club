7d702ce90e229bf1382cc7c28fcabe35b27a2b8e
sethhi2 <sethcram@gmail.com>
2023-01-25 20:36:22 -0800

    login route finished

--- api/routes/Auth.js
+++ api/routes/Auth.js
@@ -2,6 +2,7 @@ const Users = require("./Users");
 const router = require("express").Router(); //can handle post, put (update), get, delete
 const user = require("../models/User");
 const bcrypt = require('bcrypt');
+const { request } = require("express");
 
 //Register
 router.post("/register", async (request, response) => { //async bc dont know how long it'll take
@@ -23,5 +24,20 @@ router.post("/register", async (request, response) => { //async bc dont know how
 }); 
 
 //Login
+router.post("/login", async (request, response) => {
+    try {
+        const user = await Users.findOne({ username: request.body.username })
+        !user && response.status(400).json("Wrong username or password")
+
+        const validated = await bcrypt.compare(request.body.password, user.password) //compare passed in and pass + stored pass
+        !validated && response.status(400).json("Wrong username or password")
+
+        const { password, ...others } = user.__doc;
+        response.status(200).json(others)
+    }
+    catch (error) {
+        response.status(500).json(error);
+    }
+})
 
 module.exports = router
