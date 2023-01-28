09aab7ece9769452a1b0af3c28ca46b63a52f257
sethhi2 <sethcram@gmail.com>
2023-01-28 15:30:19 -0800

    progress on editing post, but cant change yet

--- client/src/components/singlepost/SinglePost.jsx
+++ client/src/components/singlepost/SinglePost.jsx
@@ -10,6 +10,9 @@ export default function SinglePost() {
     const postId = location.pathname.split("/")[2]; //get the post id
     const [post, setPost] = useState([]);
     const { user } = useContext(Context);
+    const [title, setTitle] = useState("");
+    const [description, setDecription] = useState("");
+    const [updateMode, setUpdateMode] = useState(false);
 
     //retrieve post according to postId
     useEffect(() => {
@@ -43,15 +46,19 @@ export default function SinglePost() {
                     alt="" 
                 />
               )}
-              <h1 className="singlePostTitle">
-                  {post.title}
-                  {post.username === user?.username && // ? indicates only do comparison if user != null
-                    <div className="singlePostIcons">
-                        <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
-                        <i className="singlePostIcon fa-regular fa-trash-can" onClick={handleDelete}></i>
-                    </div>
-                  }
-              </h1>
+              
+              {updateMode ? <input type="text" value={post.title} className="singlePostTitleInput" autoFocus="true" /> : 
+                <h1 className="singlePostTitle">
+                    {post.username === user?.username && // ? indicates only do comparison if user != null
+                        <div className="singlePostIcons">
+                            <i className="singlePostIcon fa-regular fa-pen-to-square" onClick={()=>setUpdateMode(true)}></i>
+                            <i className="singlePostIcon fa-regular fa-trash-can" onClick={handleDelete}></i>
+                        </div>
+                    }
+                </h1>
+              }
+                  
+              
               <div className="singlePostInfo">
                   <span className="singlePostAuthor">
                       Author:
@@ -63,9 +70,11 @@ export default function SinglePost() {
                   </span>
                   <span className="singlePostDate">Published: <b>{new Date(post.createdAt).toDateString()}</b></span>
               </div>
-              <p className="singlePostDescription">
-                {post.description}
-              </p>
+              {updateMode ? <input type="text" value={post.description} className="singlePostDescriptionInput"/> :
+                <p className="singlePostDescription">
+                    {post.description}
+                </p>
+              }
           </div>
       </div>
   )
