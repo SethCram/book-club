b59a57812fe650101dc89408f054fc49672749f9
sethhi2 <sethcram@gmail.com>
2023-01-28 11:41:23 -0800

    write page progress but not working

--- client/src/pages/writepage/WritePage.jsx
+++ client/src/pages/writepage/WritePage.jsx
@@ -1,14 +1,56 @@
+import { useContext, useState } from "react"
 import "./WritePage.css"
+import axios from "axios"
+import { Context } from "../../context/Context";
 
 export default function WritePage() {
-  return (
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [picture, setPicture] = useState(null);
+    const { user } = useContext(Context);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const newPost = {
+            username: user.username,
+            title: title,
+            description,
+        };
+
+        if (picture) {
+            const data = new FormData();
+            const fileName = Date.now() + picture.name;
+
+            data.append("name", fileName);
+            data.append("file", picture);
+
+            newPost.photo = fileName;
+
+            try {
+                await axios.post("/upload", data);
+            } catch (error) {
+                
+            }
+        }
+        try {
+            const response = await axios.post("/posts", newPost); // errors out here
+            window.location.replace("/singlePostPage/" + response.data._id);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
       <div className='writePage'>
-          <img 
+          {picture && 
+            <img 
               className="writeImg"
-              src="https://upload.wikimedia.org/wikipedia/en/3/37/Dog_with_a_Blog_Logo.png"
+              src={URL.createObjectURL(picture)}
               alt=""
-          />
-          <form className='writeForm'>
+            />
+          }
+          <form className='writeForm' onSubmit={handleSubmit}>
               <div className='writeFormGroup'>
                   <label htmlFor='fileInput'>
                     <i className="writeIcon fa-solid fa-plus"></i>
@@ -17,18 +59,26 @@ export default function WritePage() {
                       type='file'
                       id='fileInput'
                       style={{ display: "none" }}
+                      onChange={event=>setPicture(event.target.files[0])} //set picture to file uploaded
                   />
                   <input
                       className='writeInput'
                       type='text'
                       placeholder='Title'
                       autoFocus={true}
+                      onChange={event=>setTitle(event.target.title)}
                   />
               </div>
               <div className="writeFormGroup">
-                  <textarea className="writeInput writeText" placeholder="Tell your story..." type="text"></textarea>
+                <textarea 
+                    className="writeInput writeText" 
+                    placeholder="Tell your story..." 
+                    type="text"
+                    onChange={event=>setDescription(event.target.description)}
+                >
+                </textarea>
               </div>
-              <button className="writeSubmit">Publish</button>
+              <button className="writeSubmit" type="submit">Publish</button>
           </form>
     </div>
   )
