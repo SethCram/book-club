ca5ace09fc34c5f2ce6ceee9e0bf3dd34ad53f0c
sethhi2 <sethcram@gmail.com>
2023-02-16 16:37:52 -0800

    fixed improper cursor pointer

--- client/src/components/socialmediaicons/SocialMediaIcons.jsx
+++ client/src/components/socialmediaicons/SocialMediaIcons.jsx
@@ -15,10 +15,10 @@ export default function SocialMediaIcons({user, barPosition}) {
     const topbar = barPosition === "top";
     return (      
     <div>
-        {generateIcon(user?.instagramLink, "fa-brands fa-square-instagram", topbar)}
-        {generateIcon(user?.twitterLink, "fa-brands fa-square-twitter", topbar)}
-        {generateIcon(user?.facebookLink, "fa-brands fa-square-facebook", topbar)}
-        {generateIcon(user?.pinterestLink, "fa-brands fa-square-pinterest", topbar)}
+        {user && generateIcon(user?.instagramLink, "fa-brands fa-square-instagram", topbar)}
+        {user && generateIcon(user?.twitterLink, "fa-brands fa-square-twitter", topbar)}
+        {user && generateIcon(user?.facebookLink, "fa-brands fa-square-facebook", topbar)}
+        {user && generateIcon(user?.pinterestLink, "fa-brands fa-square-pinterest", topbar)}
     </div>
   )
 }
