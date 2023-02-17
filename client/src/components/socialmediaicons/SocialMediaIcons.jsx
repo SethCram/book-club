7e31a5ba3cdc1654aef708bd62ae2d45fc4fe81f
sethhi2 <sethcram@gmail.com>
2023-02-16 16:28:54 -0800

    fixed social media link areas

--- client/src/components/socialmediaicons/SocialMediaIcons.jsx
+++ client/src/components/socialmediaicons/SocialMediaIcons.jsx
@@ -1,43 +1,24 @@
 import "./SocialMediaIcons.css"
 
+const generateIcon = (link, classNames, topbar) => (
+    <i className={(topbar ? "topIcon" : "sidebarIcon")}>
+        <a
+            className={`${classNames} link`}
+            href={link}
+            target="_blank"
+            rel="noreferrer"
+        />
+    </i>
+);
+
 export default function SocialMediaIcons({user, barPosition}) {
     const topbar = barPosition === "top";
     return (      
     <div>
-        {user?.instagramLink &&
-            <a className='link' href={user.instagramLink}>
-                <i
-                    className={"fa-brands fa-square-instagram " + (topbar ? "topIcon" : "sidebarIcon")}
-                    target="_blank"
-                />
-            </a>
-        }
-        {user?.twitterLink &&
-            <a className='link' href={user.twitterLink}>
-                <i
-                    className={"fa-brands fa-square-twitter " + (topbar ? "topIcon" : "sidebarIcon")} 
-                    target="_blank"
-                />
-            </a>
-        }
-        {user?.facebookLink &&
-            <a className='link' href={user.facebookLink}>
-                <i
-                    className={"fa-brands fa-square-facebook " + (topbar ? "topIcon" : "sidebarIcon")}
-                    target="_blank"
-                    rel="noreferrer"
-                />
-            </a>
-        }
-        {user?.pinterestLink &&
-            <a className='link' href={user.pinterestLink}>
-                <i
-                    className={"fa-brands fa-square-pinterest " + (topbar ? "topIcon" : "sidebarIcon")}
-                    target="_blank"
-                    rel="noreferrer"
-                />
-            </a>
-        }
+        {generateIcon(user?.instagramLink, "fa-brands fa-square-instagram", topbar)}
+        {generateIcon(user?.twitterLink, "fa-brands fa-square-twitter", topbar)}
+        {generateIcon(user?.facebookLink, "fa-brands fa-square-facebook", topbar)}
+        {generateIcon(user?.pinterestLink, "fa-brands fa-square-pinterest", topbar)}
     </div>
   )
 }
