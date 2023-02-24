539ae5f64aa652ef9204daa954ff1642d1c9cee7
sethhi2 <sethcram@gmail.com>
2023-02-23 19:20:18 -0800

    fix error 404 link, wording and coloring

--- client/src/pages/notfound/NotFound.jsx
+++ client/src/pages/notfound/NotFound.jsx
@@ -6,17 +6,19 @@ export default function NotFound() {
     <div className="notFound">
         <div className='notFoundWrapper'>
             <img src={logo} className="notFoundLogo" alt="" />
-            <p className='notFoundDetails'>
+            <p className='notFoundItem notFoundError'>
                 Error 404: Page Not Found
             </p>
-            <a 
-                className="notFoundLink"
-                href="https://github.com/SethCram/book-club/issues/new/choose"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Help Us Out
-            </a>
+            <div className="notFoundItem"> 
+                <a 
+                    className='notFoundLink'
+                    href="https://github.com/SethCram/book-club/issues/new/choose"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Get Help
+                </a>
+            </div>
         </div>
     </div>
   )
