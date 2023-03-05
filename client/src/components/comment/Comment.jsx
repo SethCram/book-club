cf6dddecf38a084151160074ef1ef9ab51ffe50a
sethhi2 <sethcram@gmail.com>
2023-03-04 16:30:21 -0800

    allow replying to other comments thru comp comms

--- client/src/components/comment/Comment.jsx
+++ client/src/components/comment/Comment.jsx
@@ -4,44 +4,10 @@ import axios from "axios"
 import { Context } from "../../context/Context";
 import ReputationIcon from "../reputationIcon/ReputationIcon";
 
-export default function Comment({ postId, comment = null, reply = false, template = false }) {
-    const [replyIndex, setReplyIndex] = useState(-1); //init to outside arr bounds
-    const [replyId, setReplyId] = useState("");
+export default function Comment({ handleComment = null, handleReply = null, comment = null, replyId = "", replyUsername = ""}) {
     const [feedback, setFeedback] = useState("");
     const { user } = useContext(Context);
     const [vote, setVote] = useState(null);
-
-    const handleCommment = async (event, replyId = "", replyUsername = "") => {
-        event.preventDefault();
-        
-        const newComment = {
-            username: user.username,
-            postId,
-            description: feedback
-        };
-
-        if (replyId) {
-            newComment["replyId"] = replyId;
-        }
-
-        if (replyUsername) {
-            newComment["replyUsername"] = replyUsername;
-        }
-
-        try {
-            const response = await axios.post("/comments/", newComment); 
-
-            //refetch all comments
-            //await getComments();
-
-            //setLocalComments()
-
-            console.log(response);
-        } catch (error) {
-            
-        }
-        
-    };
 
     const chooseVoteIconClass = (desiredNumber, clearIcon) => {
 
@@ -71,107 +37,109 @@ export default function Comment({ postId, comment = null, reply = false, templat
     };
 
   return (
-      <div className={`commentContainer ${reply ? "commentReplyContainer" : ""}`}>
-        {/* author profile pic? */}
+    <div className="comment">
+        <div className={`commentContainer ${replyUsername ? "commentReplyContainer" : ""}`}>
+            {/* author profile pic? */}
 
-        <div className="commentIcons">
-            <ReputationIcon
-                repScore={comment ? comment.reputation : 0}
-                comment={comment ? comment : { badgeName: ""}}
-                numberClass="commentRepNumbering"
-            />
-            {comment && //display icons if a pre-existing comment
-                <>
-                    <div className="singlePostScoringIconCommentPairing lock">
-                        <div className="singlePostScoringIconComment">
-                            <i
-                                className={`${chooseVoteIconClass(0, true)} fa-regular fa-thumbs-up`}
-                                onClick={() => {/*handleVote(clearThumbsUpScore) */ }}
-                            ></i>
-                        </div>
-                        <div className="singlePostScoringIconComment">
-                            <i
-                                className={`${chooseVoteIconClass(1, false)} fa-solid fa-thumbs-up`}
-                                onClick={() => {/*handleVote(solidThumbsUpScore) */ }}
-                            ></i>
-                        </div>
-                    </div>
-                    <div className="singlePostScoringIconCommentPairing lock">
-                        <div className="singlePostScoringIconComment">
-                            <i
-                                className={`${chooseVoteIconClass(0, true)} fa-regular fa-thumbs-down`}
-                                onClick={() => {/*handleVote(clearThumbsDownScore) */ }}
-                            ></i>
-                        </div>
-                        <div className="singlePostScoringIconComment">
-                            <i
-                                className={`${chooseVoteIconClass(-1, false)} fa-solid fa-thumbs-down`}
-                                onClick={() => {/*handleVote(solidThumbsDownScore) */ }}
-                            ></i>
-                        </div>
-                    </div>
-                </>
-            }
-        </div>
-        
-        <div className="commentContent">
-            <span className="commentTitleRow">
-                
-                <h3 className="commentAuthor">
-                    {comment ? comment.username: user.username}
-                </h3>
-                {comment && new Date(comment.updatedAt).toDateString()}
-            </span>
-            {reply &&
-                <span className="commentReplyTo">
-                    @{comment?.replyUsername}
-                </span>
-            }
-            {comment ? (
-                <p className="commentDescription">
-                    {comment.description}
-                  </p>
-              )
-                :
-                  (
-                    <textarea 
-                        className="commentTextArea"
-                        placeholder="Enter your feedback..."
-                        value={feedback}
-                        onChange={text => setFeedback(text.target.value)}
-                        required
-                    />    
-                )
-            }
-            <span className="commentBottomRow">
-                {comment ? (
-                    <button
-                        className="commentReply"
-                        onClick={() => {/* setReplyIndex(i); */ setReplyId(comment._id); }}
-                    >
-                        Reply
-                    </button>
-                  ) : (
+            <div className="commentIcons">
+                <ReputationIcon
+                    repScore={comment ? comment.reputation : 0}
+                    comment={comment ? comment : { badgeName: ""}}
+                    numberClass="commentRepNumbering"
+                />
+                {comment && //display icons if a pre-existing comment
                     <>
-                        <button
-                            className="commentReply"
-                            onClick={(event) => handleCommment(event)}
-                            type="submit"
-                        >
-                            Confirm
-                        </button>
-                        <button
-                            className="commentReply"
-                            onClick={() => {setFeedback("")}}
-                        >
-                            Cancel
-                        </button>  
-                    </>          
-                  )
+                        <div className="singlePostScoringIconCommentPairing lock">
+                            <div className="singlePostScoringIconComment">
+                                <i
+                                    className={`${chooseVoteIconClass(0, true)} fa-regular fa-thumbs-up`}
+                                    onClick={() => {/*handleVote(clearThumbsUpScore) */ }}
+                                ></i>
+                            </div>
+                            <div className="singlePostScoringIconComment">
+                                <i
+                                    className={`${chooseVoteIconClass(1, false)} fa-solid fa-thumbs-up`}
+                                    onClick={() => {/*handleVote(solidThumbsUpScore) */ }}
+                                ></i>
+                            </div>
+                        </div>
+                        <div className="singlePostScoringIconCommentPairing lock">
+                            <div className="singlePostScoringIconComment">
+                                <i
+                                    className={`${chooseVoteIconClass(0, true)} fa-regular fa-thumbs-down`}
+                                    onClick={() => {/*handleVote(clearThumbsDownScore) */ }}
+                                ></i>
+                            </div>
+                            <div className="singlePostScoringIconComment">
+                                <i
+                                    className={`${chooseVoteIconClass(-1, false)} fa-solid fa-thumbs-down`}
+                                    onClick={() => {/*handleVote(solidThumbsDownScore) */ }}
+                                ></i>
+                            </div>
+                        </div>
+                    </>
                 }
-            </span>
+            </div>
+            
+            <div className="commentContent">
+                <span className="commentTitleRow">
+                    
+                    <h3 className="commentAuthor">
+                        {comment ? comment.username: user.username}
+                    </h3>
+                    {comment && new Date(comment.updatedAt).toDateString()}
+                </span>
+                {replyUsername &&
+                    <span className="commentReplyTo">
+                        @{replyUsername}
+                    </span>
+                }
+                {comment ? (
+                    <p className="commentDescription">
+                        {comment.description}
+                    </p>
+                )
+                    :
+                    (
+                        <textarea 
+                            className="commentTextArea"
+                            placeholder="Enter your feedback..."
+                            value={feedback}
+                            onChange={text => setFeedback(text.target.value)}
+                            required
+                        />    
+                    )
+                }
+                <span className="commentBottomRow">
+                    {comment ? (
+                        <button
+                            className="commentReply"
+                              onClick={() => { handleReply(comment._id); }}
+                        >
+                            Reply
+                        </button>
+                    ) : (
+                        <>
+                            <button
+                                className="commentReply"
+                                onClick={(event) => handleComment(event, feedback, replyId, replyUsername)}
+                                type="submit"
+                            >
+                                Confirm
+                            </button>
+                            <button
+                                className="commentReply"
+                                onClick={() => {setFeedback("")}}
+                            >
+                                Clear
+                            </button>  
+                        </>          
+                    )
+                    }
+                </span>
+            </div>
+            
         </div>
-        
     </div>
   )
 }
--- client/src/components/commentSection/CommentSection.jsx
+++ client/src/components/commentSection/CommentSection.jsx
@@ -1,11 +1,13 @@
 import "./CommentSection.css"
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import axios from "axios"
 import Comment from "../comment/Comment";
+import { Context } from "../../context/Context";
 
 export default function CommentSection({postId}) {
     const [comments, setComments] = useState([]);
-    const [replyIndex, setReplyIndex] = useState(-1); //init to outside arr bounds
+    const [replyId, setReplyId] = useState("");
+    const { user } = useContext(Context);
 
     useEffect(() => {
         const getComments = async () => {
@@ -18,21 +20,70 @@ export default function CommentSection({postId}) {
 
     }, [postId]);
 
+    const getComments = async () => {
+        const response = await axios.get("/comments/all/" + postId);
+        setComments(response.data);
+    }
+
+    const handleComment = async (event, feedback, replyToId = "", replyUsername = "") => {
+        event.preventDefault();
+        
+        const newComment = {
+            username: user.username,
+            postId,
+            description: feedback
+        };
+
+        if (replyToId) {
+            newComment["replyId"] = replyToId;
+
+            //clear reply Id to wipe out comment template
+            setReplyId("");
+        }
+
+        if (replyUsername) {
+            newComment["replyUsername"] = replyUsername;
+        }
+
+        try {
+            const response = await axios.post("/comments/", newComment); 
+
+            //refetch all comments
+            await getComments();
+
+            //setLocalComments()
+        } catch (error) {
+            
+        }
+        
+    };
+
+    const handleReply = (replyId) => {
+        //create another comment below the requested comment
+        // but how to figure out what the requested comment is?
+
+        setReplyId(replyId);
+    };
+
     return (
         <div className="commentSection">
-            <Comment postId={postId} template={true} />
+            <Comment handleComment={handleComment} postId={postId}/>
 
-            {comments.length > 0 && 
-                comments.map((comment, i) => (
+            {comments.length > 0 && //render every comment
+                comments.map((comment) => (
                     <div key={comment._id}>
-                        <Comment postId={postId} comment={comment}/>
+                        <Comment postId={postId} handleReply={handleReply} comment={comment}/>
                         
-                        {replyIndex === i && 
-                            <Comment postId={postId} reply={true} template={true} />
+                        {replyId === comment._id && 
+                            <Comment handleComment={handleComment} postId={postId} replyId={comment._id} replyUsername={comment.username}/>
                         }
-                            {comment.replies.length > 0 && comment.replies.map((reply, j) => (
+
+                        {comment.replies.length > 0 && comment.replies.map((reply) => ( //render every reply to the root comment
                             <div key = {reply._id}>
-                                <Comment postId={postId} comment={reply} reply={true} />                             
+                                <Comment postId={postId} handleReply={handleReply} comment={reply} replyId={reply.replyId} replyUsername={reply.replyUsername} />  
+                                {replyId === reply._id && 
+                                    <Comment handleComment={handleComment} postId={postId} replyId={reply._id} replyUsername={reply.username}/>
+                                }
                             </div>
                         ))
                         }
