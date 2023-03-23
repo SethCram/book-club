782a404f5f0960c7144ffeced60ccb5edbdfd910
sethhi2 <sethcram@gmail.com>
2023-03-23 13:43:45 -0700

    make ckeditor resize with screen size

--- client/src/components/editor/Editor.jsx
+++ client/src/components/editor/Editor.jsx
@@ -3,20 +3,29 @@ import { Editor as CustomEditor } from 'ckeditor5-custom-build/build/ckeditor';
 import "./Editor.css";
 
 export default function Editor({setDescription, defaultText="<p>Tell your story...</p>"}) {
+
+
     return (
         <div>
             <CKEditor
-                editor={ CustomEditor }
+                editor={CustomEditor}
                 data={defaultText}
-                onReady={ editor => {
+                onReady={editor => {
                     // You can store the "editor" and use when it is needed.
-                    console.log( 'Editor is ready to use!', editor );
-                } }
-                onChange={ ( event, editor ) => {
+                    console.log('Editor is ready to use!', editor);
+                    /*editor.editing.view.change((writer) => {
+                        writer.setStyle(
+                            "width",
+                            "200px",
+                            editor.editing.view.document.getRoot()
+                        );
+                    });*/
+                }}
+                onChange={(event, editor) => {
                     const data = editor.getData();
                     setDescription(data);
-                    console.log( { event, editor, data } );
-                } }
+                    console.log({ event, editor, data });
+                }}
             />
         </div>
   )
